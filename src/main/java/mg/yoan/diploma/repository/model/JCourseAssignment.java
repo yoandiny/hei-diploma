4a608541683cc8acd5cec586aa82a2e35dafd913
Tom-1747 <hei.damien.3@gmail.com>
2026-08-13 23:00:08 +0300

    feat: add course assignment JPA entity

---
+++ src/main/java/mg/yoan/diploma/repository/model/JCourseAssignment.java
@@ -0,0 +1,34 @@
+package mg.yoan.diploma.repository.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(
+    uniqueConstraints =
+        @UniqueConstraint(columnNames = {"course_id", "group_id", "teacher_id"}))
+@Getter
+@Setter
+public class JCourseAssignment {
+
+  @Id private String id;
+
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "course_id", nullable = false)
+  private JCourse course;
+
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "group_id", nullable = false)
+  private JGroup group;
+
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "teacher_id", nullable = false)
+  private JTeacher teacher;
+}
