d0bca50204895a274fd7add4d6f3404ff3d541ac
Tom-1747 <hei.damien.3@gmail.com>
2026-08-13 23:00:48 +0300

    feat: add exam JPA entity

---
+++ src/main/java/mg/yoan/diploma/repository/model/JExam.java
@@ -0,0 +1,30 @@
+package mg.yoan.diploma.repository.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.Instant;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class JExam {
+
+  @Id private String id;
+
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "course_id", nullable = false)
+  private JCourse course;
+
+  @Column(nullable = false)
+  private Instant dateExam;
+
+  @Column(nullable = false, precision = 5, scale = 2)
+  private BigDecimal coefficient;
+}
