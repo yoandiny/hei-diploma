a95c69b4db04fc416fa290cac18d58acf1becfeb
Tom-1747 <hei.damien.3@gmail.com>
2026-08-13 22:55:14 +0300

    feat: add group JPA entity

---
+++ src/main/java/mg/yoan/diploma/repository/model/JGroup.java
@@ -0,0 +1,27 @@
+package mg.yoan.diploma.repository.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "class_group")
+@Getter
+@Setter
+public class JGroup {
+
+  @Id private String id;
+
+  @Column(nullable = false)
+  private String ref;
+
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "promotion_id", nullable = false)
+  private JPromotion promotion;
+}
