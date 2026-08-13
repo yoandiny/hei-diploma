d7c1b3f50310bb344cd7c08d2691cb928115c711
Tom-1747 <hei.damien.3@gmail.com>
2026-08-13 22:48:15 +0300

    feat: add user JPA entity

---
+++ src/main/java/mg/yoan/diploma/repository/model/JUser.java
@@ -0,0 +1,41 @@
+package mg.yoan.diploma.repository.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import java.time.Instant;
+import lombok.Getter;
+import lombok.Setter;
+import mg.yoan.diploma.domain.Role;
+
+@Entity
+@Getter
+@Setter
+public class JUser {
+
+  @Id private String id;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private String firstName;
+
+  @Column(nullable = false)
+  private String lastName;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private Role role;
+
+  @Column(nullable = false)
+  private boolean enabled;
+
+  @Column(nullable = false)
+  private Instant createdAt;
+}
