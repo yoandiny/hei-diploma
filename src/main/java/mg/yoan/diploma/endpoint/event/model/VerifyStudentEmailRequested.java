0daf54d0c84c218e9154c30fc766e88413b2d0f6
Tom-1747 <hei.damien.3@gmail.com>
2026-08-17 19:31:44 +0300

    feat(admin): request SES verification for student emails, async (#22)
    * feat: add student email verifier
    
    * feat(service): add student email verifier
    
    * feat: add all student's email verifier

---
+++ src/main/java/mg/yoan/diploma/endpoint/event/model/VerifyStudentEmailRequested.java
@@ -0,0 +1,29 @@
+package mg.yoan.diploma.endpoint.event.model;
+
+import java.time.Duration;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class VerifyStudentEmailRequested extends PojaEvent {
+  private String studentId;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(10);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(30);
+  }
+}
