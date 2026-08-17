189f9d68811793667579f74e8d90c43f58d605d0
Tom-1747 <hei.damien.3@gmail.com>
2026-08-17 18:39:40 +0300

    feat: add all student's email verifier

--- src/main/java/mg/yoan/diploma/endpoint/rest/controller/admin/AdminStudentController.java
+++ src/main/java/mg/yoan/diploma/endpoint/rest/controller/admin/AdminStudentController.java
@@ -2,9 +2,13 @@ package mg.yoan.diploma.endpoint.rest.controller.admin;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import mg.yoan.diploma.endpoint.event.EventProducer;
+import mg.yoan.diploma.endpoint.event.model.VerifyStudentEmailRequested;
 import mg.yoan.diploma.service.StudentService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +18,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminStudentController {
 
   private final StudentService studentService;
+  private final EventProducer<VerifyStudentEmailRequested> verifyEmailEventProducer;
 
   @GetMapping
   @PreAuthorize("hasRole('ADMIN')")
   public List<StudentSummaryResponse> listStudents() {
     return studentService.listAll().stream().map(StudentSummaryResponse::from).toList();
+  }
+
+  @PostMapping("/verify-emails")
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<Void> verifyAllStudentEmails() {
+    var events =
+        studentService.listAll().stream()
+            .map(
+                student ->
+                    VerifyStudentEmailRequested.builder()
+                        .studentId(student.getId().toString())
+                        .build())
+            .toList();
+
+    if (!events.isEmpty()) {
+      verifyEmailEventProducer.accept(events);
+    }
+
+    return ResponseEntity.accepted().build();
   }
 }
